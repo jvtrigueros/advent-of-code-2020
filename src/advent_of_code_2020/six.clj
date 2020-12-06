5deaaf9d6a2c81539d44a754fb8317c545de18b4
Jose V. Trigueros <j.v.trigueros@gmail.com>
2020-12-06 08:59:38 -0800

    Finish day six

--- src/advent_of_code_2020/six.clj
+++ src/advent_of_code_2020/six.clj
@@ -1,1 +1,53 @@
-(ns advent-of-code-2020.six)
+(ns advent-of-code-2020.six
+  (:require
+    [clojure.string :as str]
+    [clojure.set :refer [intersection]]))
+
+(comment
+  (def input
+    (let [group-count (atom 0)]
+      (->> "./resources/six.txt"
+           slurp
+           str/split-lines
+           (group-by
+             (fn [l]
+               (if (str/blank? l)
+                 (swap! group-count inc))
+               @group-count))
+           vals)))
+
+  (->> (second input)
+       (filter (comp not str/blank?))
+       count)
+
+  (reduce
+    (fn [questions next-person]
+      (intersection questions
+                    (into #{} next-person)))
+    (into #{} (ffirst input))
+    (rest (first input)))
+
+  ;; part 1
+  (->> input
+       (map
+         #(reduce
+            (fn [questions next-person]
+              (into questions next-person))
+            #{}
+            %))
+       (map count)
+       (apply +))
+
+  ;; part 2
+  (->> input
+       (map
+         (partial remove str/blank?))
+       (map
+         #(reduce
+            (fn [questions next-person]
+              (intersection questions
+                            (into #{} next-person)))
+            (into #{} (first %))
+            (rest %)))
+       (map count)
+       (apply +)))
